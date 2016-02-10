dfd94f1aa96f1de56ad78ddeecb2ee3a1cc02522
Anya Novikova <pesik@ane4k.in>
2016-02-10 21:06:12 +0300

    SEO optimisation

--- js/controller.js
+++ js/controller.js
@@ -19,19 +19,6 @@ show_animated_button = function() {
                 }
 
 
-                window._gaq = window._gaq || [];
-
-                window._gaq.push(['_setAccount', 'UA-50636137-1']);
-                window._gaq.push(['_trackPageview']);
-
-
-/*TODO: remove on production
-                (function() {
-                    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                    ga.src = ('https:' == document.location.protocol ?  'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-                })();
-*/
                 $(document).unbind();
 
 };
